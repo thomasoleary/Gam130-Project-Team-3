5d43a099380c6e2e7ee1d90ca70a0e5867abe893
EthanJones2001 <EJ229959@falmouth.ac.uk>
2019-11-29 15:01:03 +0000

    Started to create raycast system for doors

---
+++ Assets/Testing/Scripts/CameraRaycasting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycasting : MonoBehaviour
+{
+
+    public float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HandleRaycast();
+        }
+
+    }
+
+    private void HandleRaycast()
+    {
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            print(hit);
+        }
+    }
+
+}
