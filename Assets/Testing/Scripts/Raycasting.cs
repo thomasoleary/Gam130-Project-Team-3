bb32fab8a263c5b8ea3e8fedca05665b85f341ca
Thomas O'Leary <TO231922@falmouth.ac.uk>
2020-02-03 18:36:29 +0000

    Experimenting with new RayCasting so that the player will be able to pick up items.

---
+++ Assets/Testing/Scripts/Raycasting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{
+    
+    public GameObject pickedUpObject;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        {
+            if(hit.collider.gameObject.tag == "pickObject")
+            {
+                pickedUpObject = hit.collider.gameObject;
+                hit.collider.gameObject.transform.parent = transform;
+                hit.collider.gameObject.transform.position = (transform.position + transform.forward);
+                hit.rigidbody.useGravity = false;
+            }
+        }
+        else
+        {
+            //pickedUpObject.transform.parent = null;
+            pickedUpObject = null;
+        }
+    }
+}
