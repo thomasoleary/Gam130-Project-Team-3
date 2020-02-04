5a7cf7c23ecfda0fae5ebcbc013bffe9e47524b3
Josh <ji229970@falmouth.ac.uk>
2020-02-04 15:34:13 +0000

    Added fan spin script but needs to be sorted

---
+++ Assets/Testing/Scripts/FanSpin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanSpin : MonoBehaviour
+{
+    private int spinSpeed;
+    [SerializeField]
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, spinSpeed, 0 * Time.deltaTime);
+    }
+}
