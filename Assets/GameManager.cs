7bbcfde8efe99778e0fee51560fffcb81d1c062d
Ethan <EJ229959@falmouth.ac.uk>
2020-02-04 16:07:25 +0000

    Working on item prices for opening doors

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool priceReached;
+
+    public int maxPriceAmount;
+    public int currentPriceAmount;
+
+    void ReachedAmount()
+    {
+        Debug.Log("Opening door");
+    }
+}
