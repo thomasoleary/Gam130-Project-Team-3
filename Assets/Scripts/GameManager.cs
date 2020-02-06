9c9fef190f3717d34522d6e1e492f10637398863
Ethan <elj2001@hotmail.co.uk>
2020-02-06 17:09:42 +0000

    Finished shop pricing mechanic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool priceReached;
+
+    public int maxPriceAmount;
+    public int currentPriceAmount;
+
+    void ReachedAmount()
+    {
+        priceReached = true;
+        Debug.Log("open door or whatever");
+    }
+
+    public void AddPrice(int amount)
+    {
+        currentPriceAmount += amount;
+
+        if(currentPriceAmount >= maxPriceAmount)
+        {
+            ReachedAmount();
+        }
+     
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int itemPrice;
+    bool canGetPicked = true;
+    GameManager gameManager;
+
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Trolley") && canGetPicked)
+        {
+            canGetPicked = false;
+            gameManager.AddPrice(itemPrice);
+        }
+    }
+
+
+}
