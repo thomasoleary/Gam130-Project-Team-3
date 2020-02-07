0f0d57a1f6d31d8b4e422ca0acb3354a30636723
AidanBow <AB229003@falmouth.ac.uk>
2020-02-07 10:07:17 +0000

    Stairs to tutorial

--- Assets/Scripts/FanSpin.cs
+++ Assets/Scripts/FanSpin.cs
@@ -8,36 +8,9 @@ public class FanSpin : MonoBehaviour
     [SerializeField]
     int spinSpeed = 20;
 
-    private GameObject target = null;
-    private Vector3 offset;
-
-    void Start()
-    {
-        target = null;
-    }
-
-    void OnTriggerStay(Collider col)
-    {
-        target = col.gameObject;
-        offset = target.transform.position - transform.position;
-    }
-
-    void OnTriggerExit(Collider col)
-    {
-        target = null;
-    }
-
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, spinSpeed, 0 * Time.deltaTime);
-    }
-
-    void LateUpdate()
-    {
-        if (target != null)
-        {
-            target.transform.position = transform.position + offset;
-        }
     }
 }
