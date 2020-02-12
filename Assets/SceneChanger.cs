20ec431d07a1282ec72149205bda505f4b6f056d
lr230289 <lr230289@falmouth.ac.uk>
2020-02-12 15:02:40 +0000

    Added a scene Loader that transitions to the next scene in the build list

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    private bool collisionEnter = false;
+
+    void Start()
+    {
+        collisionEnter = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (collisionEnter == true)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            collisionEnter = true;
+        }
+    }
+}
