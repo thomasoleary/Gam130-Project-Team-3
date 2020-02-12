29cb8d78c05155a740d5991ad60d8f3f980cf8a4
thomasoleary <to231922@falmouth.ac.uk>
2020-02-12 15:56:56 +0000

    Respawn.cs now changed so that the scene restarts rather than Player respawning

--- Assets/Testing/Aidan/Respawn.cs
+++ Assets/Testing/Aidan/Respawn.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Respawn : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    //[SerializeField] private Transform player;
 
-    [SerializeField] private Transform respawnPoint;
-
+    //[SerializeField] private Transform respawnPoint;
+    
     void OnTriggerEnter(Collider other)
     {
-        player.transform.position = respawnPoint.transform.position;
+        if(other.gameObject.tag == "Player")
+        {
+            Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+        }
     }
 }
