014ab818dfe845332bd4e9d955dab4f491dda7f5
Thomas O'Leary <TO231922@falmouth.ac.uk>
2020-02-12 17:40:32 +0000

    Had to re-make 2_Starting Room.unity because for some reason it was wiped? Also re-scripted SceneChanger.cs - just to make it more simple

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -5,29 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-
-    private bool collisionEnter = false;
-
-    void Start()
-    {
-        collisionEnter = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        if (collisionEnter == true)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
-        }
-    }
-
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            collisionEnter = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
