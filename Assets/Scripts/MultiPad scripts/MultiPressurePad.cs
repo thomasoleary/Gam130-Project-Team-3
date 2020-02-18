8b83161abd31336880b7717a78e07576c99a2007
Thomas O'Leary <to231922@falmouth.ac.uk>
2020-02-18 15:28:56 +0000

    Created new script called MultiPressurePad.cs and new folder path for these Assets/Scripts/MultiPad scripts.meta

---
+++ Assets/Scripts/MultiPad scripts/MultiPressurePad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPressurePad : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pressurePadOne;
+    [SerializeField]
+    GameObject pressurePadTwo;
+    [SerializeField]
+    GameObject pressurePadThree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
