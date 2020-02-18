43b45ae04aa4436dddcf2a3c262c8e554c90bbec
Thomas O'Leary <to231922@falmouth.ac.uk>
2020-02-18 16:38:10 +0000

    MultiPadDoor.cs now works

--- Assets/Scripts/MultiPad scripts/MultiPadDoor.cs
+++ Assets/Scripts/MultiPad scripts/MultiPadDoor.cs
@@ -15,6 +15,18 @@ public class MultiPadDoor : MonoBehaviour
     private bool pad2;
     private bool pad3;
 
+    Vector3 startingPos;
+    Vector3 endingPos;
+    private float duration = 5f;
+
+    void Start()
+    {
+        startingPos.Set(transform.position.x, transform.position.y, transform.position.z);
+        Debug.Log(startingPos);
+        endingPos.Set(startingPos[0], startingPos[1] + 4, startingPos[2]);
+        Debug.Log(endingPos);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -28,9 +40,13 @@ public class MultiPadDoor : MonoBehaviour
     private void OpenDoor()
     {
         if(pad1 && pad2 && pad3)
-            {
-                Debug.Log("All Pads activated");
-            }
+        {
+            transform.position = Vector3.Lerp(startingPos, endingPos, Time.time - duration);
+        }
+        else
+        {
+            transform.position = startingPos;
+        }
     }
     
 }
