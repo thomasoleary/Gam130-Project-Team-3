e83e187b897296bedba2873c0d0913f5c734d3c8
Thomas O'Leary <to231922@falmouth.ac.uk>
2020-02-25 15:54:32 +0000

    Added AudioSource component to MetalDetectors.prefab

---
+++ Assets/MetalDetectors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalDetectors : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player has entered Metal Detectors");
+        }
+    }
+
+
+}
