fe3f9c712278926c04294805f5f2d6961ca2ee51
Thomas O'Leary <TO231922@falmouth.ac.uk>
2020-02-27 23:57:20 +0000

    Made some adjustments to Switch.cs (Changed Update() to FixedUpdate()  )

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Switch : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class Switch : MonoBehaviour
         switchAnimation = GetComponentInChildren<Animator>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector3 fwd = playerCamera.transform.TransformDirection(Vector3.forward);
 
