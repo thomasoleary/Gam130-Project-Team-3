96f3d3b43c3c1464ed81fc07df66a08c9163180e
Thomas O'Leary <TO231922@falmouth.ac.uk>
2020-02-27 21:46:49 +0000

    Created relevant folder structures for audio and audio mixers. MetalDetectors.cs now plays an AudioClip when player activates the trigger - BUG: AudioClip plays multiple times at the same time.

--- Assets/MetalDetectors.cs
+++ Assets/MetalDetectors.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MetalDetectors : MonoBehaviour
-{
+{   
+    [SerializeField]
+    AudioClip detectorClip;
+
+    private AudioSource detectorSource;
+    bool PlayerEntered = false;
+
+    private void Start()
+    {
+        detectorSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerStay(Collider other)
     {
         if(other.tag == "Player")
         {
             Debug.Log("Player has entered Metal Detectors");
+            PlayerEntered = true;
+            detectorSource.PlayOneShot(detectorClip);
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player has left Metal Detectors");
+            PlayerEntered = false;
         }
     }
 
