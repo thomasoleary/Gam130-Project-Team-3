3f950120af9cd114d70f3dd7a89b7e157eb283dd
ethan <EJ229959@falmouth.ac.uk>
2020-03-03 16:45:16 +0000

    Improving enemy system

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
 
     private NavMeshAgent agent;
     public GameObject player;
+    public Image deathImage;
+
 
     [SerializeField]
     private float range;
@@ -25,6 +28,14 @@ public class Enemy : MonoBehaviour
         if(distance < range)
         {
             agent.SetDestination(player.transform.position);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject == player)
+        {
+            deathImage.GetComponent<Animator>().Play("Death");
         }
     }
 
