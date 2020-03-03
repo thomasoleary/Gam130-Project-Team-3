f63d31decc3e4bb8ee10782f3ac40eec6879a099
Josh <ji229970@falmouth.ac.uk>
2020-03-03 16:58:15 +0000

    Added new test scene and got Collectables.cs working on a very simple level. Need to create a flowchart for it and link it to Fungus.

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Collectables : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scoreText;
+    
+    public int Score;
+    
+    void OnTriggerEnter(Collider other) {
+        Score += 1;
+        scoreText.GetComponent<Text>().text = "Score: " + Score;
+        Destroy(gameObject);
+    }
+
+}
