13a13d8bb0d495eb89ba005720e5e715c6b4814d
ethan <EJ229959@falmouth.ac.uk>
2020-03-03 15:47:33 +0000

    Implemented enemy following with nav mesh for the maze level

---
+++ Assets/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public GameObject player;
+
+    [SerializeField]
+    private float range;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        
+    
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if(distance < range)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
