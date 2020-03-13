4ddfed475635308fa2e78ece8f579b14b3ac26e2
Josh <ji229970@falmouth.ac.uk>
2020-03-13 11:07:44 +0000

    Created script called TextLoad.cs which will load text on the UI when the player runs into  a trigger.

---
+++ Assets/Testing/Josh/TextLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLoad : MonoBehaviour
+{
+    public GameObject textObject;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider player) {
+        if (player.gameObject.tag == "Player") {
+            textObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+
+        IEnumerator WaitForSec() {
+            yield return new WaitForSeconds(5);
+            Destroy(textObject);
+            Destroy(gameObject);
+        
+        }
+    }
+}
