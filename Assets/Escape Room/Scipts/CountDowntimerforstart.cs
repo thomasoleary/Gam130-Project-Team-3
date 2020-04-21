a4bca5638b2680222fcef6ad23f0218b1013add5
Alper Mimoglu <alpermimoglu@gmail.com>
2020-04-21 10:08:00 +0100

    added sam's intro video

---
+++ Assets/Escape Room/Scipts/CountDowntimerforstart.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CountDowntimerforstart : MonoBehaviour
+{
+    public float timeLeft = 900.0f;
+    public Text text;
+    bool clock;
+    private float mins;
+    private float secs;
+
+    void Update()
+    {
+        if (timeLeft > 0 && clock == false)
+        {
+            clock = true;
+            StartCoroutine(Wait());
+        }
+
+        if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        timeLeft -= 1;
+        UpdateTimer();
+        yield return new WaitForSeconds(1);
+        clock = false;
+    }
+
+    void UpdateTimer()
+    {
+        int min = Mathf.FloorToInt(timeLeft / 60);
+        int sec = Mathf.FloorToInt(timeLeft % 60);
+        text.GetComponent<UnityEngine.UI.Text>().text = min.ToString("00") + ":" + sec.ToString("00");
+    }
+}
